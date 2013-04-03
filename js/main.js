8043b0177fee934db90bdc911ad957d78bb92280
Dimitry Solovyov <dimituri@gmail.com>
2013-04-03 22:21:25 +0300

    Wider menu at top of page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-$('#carousel').carousel({
-  interval: 10000
+$(function() {
+
+  $('#carousel').carousel({
+    interval: 10000
+  });
+
+  var menu = $('#main-menu');
+  $(window).scroll(function() {
+    menu.toggleClass('narrow', $(window).scrollTop() > 10);
+  });
+
 });
