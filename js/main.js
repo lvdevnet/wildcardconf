ab77ce53377fe41471fbfcd612683e587c472e2a
Dimitry Solovyov <dimituri@gmail.com>
2013-04-19 02:23:45 +0300

    Short location page w/ placeholders

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ $(function() {
   $('#carousel').carousel({
     interval: 10000
   });
+  $('.location .carousel').carousel({
+    interval: 5000
+  });
 
   var menu = $('#main-menu');
   $(window).scroll(function() {
